0e7d14582d9c26d40f524e31d18d29593ceaffe6
Michal Miskolci <misoblack300@gmail.com>
2025-02-27 23:50:38 +0100

    Funkcny kod odovzdany v stvrtok

---
+++ src/main.cpp
@@ -0,0 +1,150 @@
+#include <Arduino.h>
+#include <Servo.h>
+
+const int pinPump[] = {2,4,7,8};
+const int pinServo = 3;
+const int pinLed1[] = {5,6,10};
+const int trigPin = A1;
+const int echoPin = A0;
+
+
+Servo myservo;  // create Servo object to control a servo
+void pumpPWM(int i, int time);
+void pump(int i, bool on);
+void led1(int r, int g, int b);
+void colorLed();
+void trasiem();
+
+int state = 0;
+
+void led1(int r, int g, int b){
+  analogWrite(pinLed1[0], r);
+  analogWrite(pinLed1[1], g);
+  analogWrite(pinLed1[2], b);
+}
+
+void pump(int i, bool on){
+  digitalWrite(pinPump[i], on ? HIGH : LOW);
+}
+
+
+void pumpPWM(int i, int time){
+  if(time < 100) {
+    digitalWrite(pinPump[i], LOW);
+  } else {
+    digitalWrite(pinPump[i], HIGH);
+    delay(time);
+    digitalWrite(pinPump[i], LOW);
+    delay(time);
+  }
+}
+
+float distanceRead(){
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  float duration = pulseIn(echoPin, HIGH);
+  return ((duration*.0343)/2);
+}
+void trasiem(){
+  myservo.write(80);
+  delay(200);
+  myservo.write(120);
+  delay(200);
+}
+void setup() {
+  myservo.attach(pinServo);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(13, OUTPUT);
+  pinMode(pinLed1[0], OUTPUT);
+  pinMode(pinLed1[1], OUTPUT);
+  pinMode(pinLed1[2], OUTPUT);
+  for(int i = 0; i < 4; i++){
+    pinMode(pinPump[i], OUTPUT);
+  }
+  pump(3, false);
+  pump(2, false);
+  pump(1, false);
+  Serial.begin(9600);
+  while(distanceRead() > 10){
+    delay(100);
+  }
+  //led1(50,50,50);
+}
+
+
+static byte endColor[3];
+static byte startColor[3];
+static bool transitioning = false;
+int transitionTime;
+unsigned long transitionStart;
+
+void transitionColor(byte r, byte g, byte b, int ms) {
+  startColor[0] = endColor[0];
+  startColor[1] = endColor[1];
+  startColor[2] = endColor[2];
+  endColor[0] = r;
+  endColor[1] = g;
+  endColor[2] = b;
+  transitionStart = millis();
+  transitionTime = ms;
+  transitioning = true;
+
+
+}
+
+void colorRun() {
+  if (transitioning) {
+    float progress = (float)(millis() - transitionStart) / transitionTime;
+    if (progress >= 1.0) {
+      progress = 1.0;
+      transitioning = false;
+    }
+    
+    int r = startColor[0] + progress * (endColor[0] - startColor[0]);
+    int g = startColor[1] + progress * (endColor[1] - startColor[1]);
+    int b = startColor[2] + progress * (endColor[2] - startColor[2]);
+    led1(r, g, b);
+  }
+}
+
+long lastTransition = -5000;
+
+void loop() {
+    // if (millis() - lastTransition >= 1500) {
+    //   if (state == 0) {
+    //     transitionColor(0, 0, 80, 2000);
+    //   } else {
+    //     transitionColor(255, 0, 0, 2000);
+    //   }
+      
+    //   lastTransition = millis();
+    //   state = !state;
+    //   Serial.println("Transitioning");
+    // }
+    // colorRun();
+    pump(2, true);
+    pump(1, true);
+    pump(3, true);
+    led1(255, 0, 0);
+    delay(500);
+    led1(0, 255, 0);
+    delay(500);
+    led1(0, 0, 255);
+    delay(500);
+    trasiem();
+    // pump(3, true);
+    // delay(5000);
+    // pump(1, true);
+    // delay(5000);
+    // pump(3, false);
+    
+    // pump(1, false);
+    // delay(5000);
+    // Serial.println(distanceRead());
+  
+  }
+
